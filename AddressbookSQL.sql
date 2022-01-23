f184bff2b3a2d257bc6093763c035aacaea625fa
Your Name <akhilkumarchandragiri@gmail.com>
2022-01-23 11:03:55 +0530

    [Akhil] table of DB uc2

--- AddressbookSQL.sql
+++ AddressbookSQL.sql
@@ -1,2 +1,15 @@
 --uc1
 create database Address_Book_Service;
+--uc2
+create table Address_Books(
+Id int identity(1,1) primary key,
+first_name varchar(150),
+last_name varchar(150),
+Address varchar(150),
+City varchar(50),
+State varchar(50),
+ZipCode int,
+Phone_No int,
+Email_Id varchar(255)
+);
+select * from Address_Books;
